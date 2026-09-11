5d152cf23f114994934accd3a3d27829d486801d
susayold <susayold@gmail.com>
2026-09-12 04:48:21 +0700

    Use DuckDB-compatible quantity percentile audit

--- sql/04_facts/01_fct_transaction_line.sql
+++ sql/04_facts/01_fct_transaction_line.sql
@@ -1,4 +1,8 @@
 CREATE OR REPLACE TABLE fct_transaction_line AS
+WITH thresholds AS (
+    SELECT QUANTILE_CONT(quantity_raw, 0.99) AS p99_quantity
+    FROM stg_transaction
+)
 SELECT
     ROW_NUMBER() OVER () AS transaction_line_id,
     t.*,
@@ -8,7 +12,8 @@ SELECT
       + t.coupon_match_discount_value AS gross_spend_before_recorded_discounts,
     CASE
         WHEN t.quantity_raw <= 0 THEN TRUE
-        WHEN t.quantity_raw > PERCENTILE_CONT(0.99) WITHIN GROUP (ORDER BY t.quantity_raw) OVER () THEN TRUE
+        WHEN t.quantity_raw > q.p99_quantity THEN TRUE
         ELSE FALSE
     END AS quantity_outlier_flag
-FROM stg_transaction t;
+FROM stg_transaction t
+CROSS JOIN thresholds q;
