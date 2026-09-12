ca383de58b896b483b31866c4fa1b2c77f182477
susayold <susayold@gmail.com>
2026-09-12 09:02:35 +0700

    Require full campaign window for during observability

--- sql/04_facts/04_fct_campaign_exposure.sql
+++ sql/04_facts/04_fct_campaign_exposure.sql
@@ -12,7 +12,7 @@ SELECT DISTINCT
     c.start_day,
     c.end_day,
     c.start_day - 28 >= o.min_day AS pre_28d_observable,
-    c.start_day <= o.max_day AND c.end_day >= o.min_day AS during_observable,
+    c.start_day >= o.min_day AND c.end_day <= o.max_day AS during_observable,
     c.end_day + 14 <= o.max_day AS post_14d_observable,
     c.end_day + 28 <= o.max_day AS post_28d_observable
 FROM stg_campaign_exposure e
